950ce0047605ee9ccb1bde8f4458f74af906a057
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-13 15:31:45 -0400

    Working on nested routes, and going to start forms for recipes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :categories
-  resources :recipes
-
+  resources :categories do
+    resources :recipes
+  end
 
 
   root 'home#index'
