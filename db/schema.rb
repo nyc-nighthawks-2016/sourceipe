78f3f68226fd306e9f503338e56f87b9e7416d5a
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-13 12:40:15 -0400

    Up to date with everyone else. Now have access to users, and sessions info.

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713153914) do
+ActiveRecord::Schema.define(version: 20160713154418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -42,5 +30,15 @@ ActiveRecord::Schema.define(version: 20160713153914) do
 
   add_index "recipes", ["catergory_id"], name: "index_recipes_on_catergory_id", using: :btree
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
