241e8de592b725594b06f905d10dfaef64ed0133
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-13 12:04:11 -0400

    Creates controller for recipes, and begins to create the seed file information.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# What has been Seeded:
+User.delete_all
+Recipe.delete_all
+Category.delete_all
+
+#----------------------------------------------------------------------------
+# Users
+
+users = []
+100.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  users << User.create!({
+                         first_name: first_name,
+                         last_name: last_name,
+                         username: Faker::Internet.user_name("#{first_name} #{last_name[0..2]}", %w(. _ -)),
+                         email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+                         password: Faker::Internet.password(8, 15)})
+end
