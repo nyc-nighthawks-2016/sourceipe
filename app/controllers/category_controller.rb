e06734fa103678d27710e5e065c66b06ebbc27b5
jsgordon212 <jsgordon212@gmail.com>
2016-07-13 17:16:19 -0400

    added categories to home/index

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,7 +1,6 @@
 class CategoryController < ApplicationController
 
   def index
-
     @appetizers = Category.find_by_id(1)
     @salads = Category.find_by_id(2)
     @entrees = Category.find_by_id(3)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-    @categories = Category.all
+    @appetizers = Category.find_by_id(1)
+    @salads = Category.find_by_id(2)
+    @entrees = Category.find_by_id(3)
+    @desserts = Category.find_by_id(4)
+
   end
 end
