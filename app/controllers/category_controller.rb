d9fdb6d5656fbbc92c7ee9e4fe26aaa0e57670bd
jsgordon212 <jsgordon212@gmail.com>
2016-07-13 15:20:34 -0400

    added catorgies show page

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,10 +1,12 @@
 class CategoryController < ApplicationController
 
   def index
+
     @appetizers = Category.find_by_id(1)
     @salads = Category.find_by_id(2)
     @entrees = Category.find_by_id(3)
     @deserts = Category.find_by_id(4)
+
   end
 
   def show
