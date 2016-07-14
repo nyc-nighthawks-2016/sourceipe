a124d67578bc9c56cd073ee6d97e3ac0ef61e49c
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-14 16:21:32 -0400

    Begins to work on ratings algorithm, begins to add new instance variables to the home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,10 @@ class HomeController < ApplicationController
     @salads = Category.find_by(name: "Salads")
     @entrees = Category.find_by(name: "Entrees")
     @desserts = Category.find_by(name: "Desserts")
+
+    # @top_appetizers = top_rated(Category.find_by(name: "Appetizers").recipes)
+    # @top_salads = top_rated(Category.find_by(name: "Salads").recipes)
+    # @top_entrees = top_rated(Category.find_by(name: "Entrees").recipes)
+    # @top_desserts = top_rated(Category.find_by(name: "Desserts").recipes)
   end
 end
