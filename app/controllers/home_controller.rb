3f12c04543e8836fddaa069615cbefcde46fbaac
jsgordon212 <jsgordon212@gmail.com>
2016-07-14 18:02:51 -0400

    added sort method to index controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @appetizers = Category.find_by(name: "Appetizers")
+    @appetizers = Category.find_by(name: "Appetizers").top_recipes
     @salads = Category.find_by(name: "Salads")
     @entrees = Category.find_by(name: "Entrees")
     @desserts = Category.find_by(name: "Desserts")
