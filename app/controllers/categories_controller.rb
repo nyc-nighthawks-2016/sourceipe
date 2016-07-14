0f302ee5097315dc51a2c383bf541b0eebb7e569
jsgordon212 <jsgordon212@gmail.com>
2016-07-13 20:26:45 -0400

    add recipe sow page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @appetizers = Category.find_by(name: "Appetizers")
-    @salads = Category.find_by(name: "Salads")
-    @entrees = Category.find_by(name: "Entrees")
-    @desserts = Category.find_by(name: "Desserts")
+    @categories = Category.all
   end
 
   def show
