6ff4a7026e20e9404cfd5939c9d98253971dfbd0
Brian Lee <brian.lee.1088@gmail.com>
2016-07-14 17:32:49 +0000

    Add casecommons gem and add code to make recipe name and ingredient name searchable

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,11 @@ class Recipe < ActiveRecord::Base
   has_many :measurements, through: :components
 
   validates :name, :difficulty, :prep_time, :directions, :author, presence: true
+  
+  include PgSearch
+  multisearchable :against => [:name]
+
+  def self.rebuild_pg_search_documents
+    find_each { |record| record.update_pg_search_document }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713195212) do
+ActiveRecord::Schema.define(version: 20160714155609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,27 @@ ActiveRecord::Schema.define(version: 20160713195212) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "value",      null: false
+    t.integer  "recipe_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "ratings", ["recipe_id"], name: "index_ratings_on_recipe_id", using: :btree
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id", using: :btree
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name",        null: false
