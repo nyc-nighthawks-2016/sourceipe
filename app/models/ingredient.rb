4b7d1e24be3387c49252a94c1c9b22ec4cb5638d
jsgordon212 <jsgordon212@gmail.com>
2016-07-15 12:10:47 -0400

    added validation to ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,15 @@
 class Ingredient < ActiveRecord::Base
   has_many :components
   has_many :recipes, through: :components
+
   validates :name, presence: true
+
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+
+  validates :name, presence: true
+  validates :ingredient_exclude
+
 
   include PgSearch
   multisearchable :against => [:name]
@@ -9,4 +17,11 @@ class Ingredient < ActiveRecord::Base
   def self.rebuild_pg_search_documents
     find_each { |record| record.update_pg_search_document }
   end
+
+  def ingredient_exclude
+    unless self.name !~ /^[a-zA-Z\s]*$/
+      errors.add(:name, "must contain letters only")
+    end
+  end
+
 end
